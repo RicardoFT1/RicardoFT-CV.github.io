7c4b88cabf0712c74718cab953e901574074877d
RicardoFT1 <ricardo.fandinot@ecci.edu.co>
2022-10-20 16:46:46 -0500

    Actualizacion

--- JS/UX.js
+++ JS/UX.js
@@ -1,3 +1,14 @@
-document.querySelector('h2')
-console.log(document.querySelector('h2'))
-console.log(document.querySelector('h1'))
+function ChangeLanguage()
+{
+/* Para obtener el valor */
+var cod = document.getElementById("Language").value;
+/* Para obtener el texto */
+var combo = document.getElementById("Language");
+var selected = combo.options[combo.selectedIndex].text;
+if(selected=="Español"){
+    location.href="es/index.html";
+}
+else{
+    location.href="../index.html";
+}
+}
